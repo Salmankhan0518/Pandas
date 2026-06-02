18d471162f80f20fab1ef4aa1c70f8ea53971abd
Salmankhan0518 <salmansharif0518@gmail.com>
2026-06-02 14:44:44 +0500

    complte read data dif f files

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+# read data from CVS file into a dataframe
+# df = pd.read_csv("sales_data_sample.csv", encoding="latin1")
+# df = pd.read_excel("SampleSuperstore.xlsx")
+df = pd.read_json("sample_Data.json")
+
+print(df)
