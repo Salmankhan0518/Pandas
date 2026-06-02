55e8ea8a692a6414660e43235729fa36874169d1
Salmankhan0518 <salmansharif0518@gmail.com>
2026-06-02 15:05:29 +0500

    read csv,json and xlsx

--- save.py
+++ save.py
@@ -1,1 +1,16 @@
 import pandas as pd
+
+data = {
+    "Name": ["Varyam", "Saif", "Salman"],
+    "Age": [10, 20, 30],
+    "City": ["Lahore", "Faisalabad", "Islamabad"]
+}
+
+
+df = pd.DataFrame(data)
+
+print(df)
+
+# df.to_csv("output.csv", index=False)
+df.to_excel("output.xlsx", index=False)
+df.to_json("output.json", index=False)
