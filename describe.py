ec19436970a5a46444314d2d48a47d3e2864c683
Salmankhan0518 <salmansharif0518@gmail.com>
2026-06-02 17:06:21 +0500

    describe method

---
+++ describe.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "Name": ["Varyam", "Saif", "Salman", "Haris", "Asad", "Shawaiz", "Ahmad", "Zain"],
+    "Age": [10, 20, 30, 40, 50, 60, 55, 70],
+    "Salary": [50000, 60000, 70000, 80000, 90000, 450000, 850000, 650000],
+    "Performance Score": [85, 90, 78, 92, 88, 95, 80, 89]
+}
+
+df = pd.DataFrame(data)
+print("Sample Dataframe")
+print(df)
+print("Descriptive Statistics")
+print(df.describe())
