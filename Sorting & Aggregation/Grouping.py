a9e34e0b5a12d33d9ef6a2c08210608aebf82d48
Salmankhan0518 <salmansharif0518@gmail.com>
2026-06-03 18:14:12 +0500

    Grouping on sinle column

---
+++ Sorting & Aggregation/Grouping.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = {
+    "Name": ["Salman", "Saif", "Zain", "Asad", "Shawaiz"],
+    "Age": [28, 34, 22, 34, 28],
+    "Salary": [50000, 60000, 45000, 52000, 480000]
+}
+
+df = pd.DataFrame(data)
+
+print("Grouped Data")
+grouped = df.groupby("Age")["Salary"].sum()
+print(grouped)
+
+
